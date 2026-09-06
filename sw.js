bbcca704553c5a8f44e9681eda5d8efc53e39843
vencandodaya-ctrl <vencandodaya@gmail.com>
2026-09-06 19:01:44 +0630

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = "pali-speaking-v3";
+const APP_SHELL = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./pali-icon-192.png",
+  "./pali-icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+    ))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request))
+  );
+});
